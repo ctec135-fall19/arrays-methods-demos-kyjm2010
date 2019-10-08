af8760d69bc386c07cb1ff400b27f3c59069f2d3
k.mcdonald2 <k.mcdonald2@SHL125-19>
2019-10-08 15:18:00 -0700

    Lab 3 done

---
+++ TuesdayDemo10_08/Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring
+            int[] myArray = new int[5];
+
+            //initializing
+            for(int i =0; i < myArray.Length; i++)
+            {
+                myArray[i] = i + 10;
+            }
+
+            //traversing
+            Console.WriteLine("print array elements");
+            foreach (int element in myArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ TuesdayDemo10_08/DecisionConstructs/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionConstructs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (true)
+            {
+                Console.WriteLine(true);
+            }
+            if (false)
+            {
+                Console.WriteLine(true);
+            }
+
+            if (false)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine("if/esle {0}", false);
+            }
+            #region switch statemennt example
+            for (int i = 0; i<6; i ++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Console.WriteLine(i);
+                        break;
+                    case 1:
+                        Console.WriteLine(i);
+                        break;
+                    case 2:
+                    case 3:
+                        Console.WriteLine("2 or 3: {0}", i);
+                        break;
+                    default:
+                        Console.WriteLine("default: {0}", i);
+                        break;
+
+                }
+            }
+            #endregion
+
+        }
+    }
+}
